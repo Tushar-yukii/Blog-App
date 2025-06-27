75d5843a8dea24880cbf94b3c9ad1dc4f024a882
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-06-27 22:29:51 +0530

    deletePost

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,5 +1,5 @@
 import conf from "../config/conf";
-import { Client, Account, ID } from "appwrite";
+import { Client, Account, ID, Query } from "appwrite";
 
 export class Service {
   client = new Client();
@@ -44,6 +44,43 @@ export class Service {
       );
     } catch (error) {
       console.log("appwrite service :: updatePost :: error", error);
+    }
+  }
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("appwrite service :: deletePost :: error", error);
+      return false;
+    }
+  }
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("appwrite service :: getPost :: error", error);
+      return false;
+    }
+  }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("appwrite service :: getPosts :: error", error);
+      return false;
     }
   }
 }
