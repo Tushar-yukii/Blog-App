91602a14022e3cf06b41626156503acf11641d5d
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-07-01 18:13:18 +0530

    Mega project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>
-      header
-    </div>
-  )
-}
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
 
-export default Header
+  const naItem = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  },
+  ];
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav>
+          <div>
+            
+          </div>
+        </nav>
+      </Container>
+
+    </header>
+  )
+};
+
+export default Header;
