e7a4d271bd0b8581050ef9c2378125e1c5b97d35
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-07-02 15:04:33 +0530

    mega project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
 
-  const naItem = [
+  const navItems = [
     {
       name: "Home",
       slug: "/",
@@ -18,35 +18,55 @@ const Header = () => {
       name: "Login",
       slug: "/login",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "Signup",
       slug: "/signup",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "All Posts",
       slug: "/all-posts",
       active: authStatus,
-  },
-  {
+    },
+    {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
-  },
+    },
   ];
   return (
     <header className="py-3 shadow bg-gray-500">
       <Container>
-        <nav>
-          <div>
-            
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo />
+            </Link>
           </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
         </nav>
       </Container>
-
     </header>
-  )
+  );
 };
 
 export default Header;
