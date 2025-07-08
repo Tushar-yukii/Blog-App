e0be2693754c32b715aa792ed1fdc4ceda912aac
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-07-08 20:51:23 +0530

    Mega project

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -5,12 +5,17 @@ import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const PostForm = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+const PostForm = ({ post }) => {
+  const { register, handleSubmit, watch, setValue, control, getVlaue } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: slug?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+  return <div>postform</div>;
 };
 
 export default PostForm;
