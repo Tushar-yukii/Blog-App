4644b96e1e8e43e65cf0bca253f6ba773ea9c0c2
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-07-11 19:22:28 +0530

    Mega project

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,21 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-export { Header, Footer, Container, Logo, LogoutBtn, RTE };
+import Sighup from "./Sighup";
+import Login from "../pages/Login";
+import Post from "./Post";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Sighup,
+  Login,
+  Post,
+  PostForm,
+  PostCard,
+};
---
+++ src/pages/Sighup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Sighup as Sighupcomponents} from '../components'
+const Sighup = () => {
+  return (
+    <div className='py-8'> 
+      <Sighupcomponents /> 
+    </div>
+  )
+}
+
+export default Sighup
