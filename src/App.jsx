44ed505cdf284a222d3bccf74e5e9cbe65373553
Tushar Tikkiwal <ttikkiwal@gmail.com>
2025-08-16 23:56:46 +0530

    Mega project

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
   }, []);
 
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-380">
       <div className="w-full block">
         <Header />
 
